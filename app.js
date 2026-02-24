a6cf3925700876af8920ea627a40abba5a96feae
Sadid Acosta <seacostao@academia.usbbog.edu.co>
2026-02-24 13:42:25 -0500

    Names

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const getAthletes = (coachId) => {
     return new Promise((resolve) => {
         setTimeout(() => {
             const athletes = [
-                { id: 1, user: "jUAn pErez", status: "inactive", points: 45 },
-                { id: 2, user: "mArIa gArCiA", status: "active", points: 88 },
-                { id: 3, user: "cArLoS rOdrIguEz", status: "inactive", points: 12 },
-                { id: 4, user: "lUciA fErNAnDeZ", status: "active", points: 95 },
-                { id: 5, user: "pAbLo mArTiN", status: "inactive", points: 30 }
+                { id: 1, user: "Sadid Acosta", status: "inactive", points: 45 },
+                { id: 2, user: "Brian Zambrano", status: "active", points: 88 },
+                { id: 3, user: "Pablo Cháves", status: "inactive", points: 12 },
+                { id: 4, user: "Andrea Rondón", status: "active", points: 95 },
+                { id: 5, user: "Miguelito el mascapito", status: "inactive", points: 30 }
             ];
             resolve(athletes);
         }, 1200);
